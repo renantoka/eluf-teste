99e537fd0b51de35943dc2cc87cfc8ff233a0a76
PedroHBCruz <75803558+PedroHBCruz@users.noreply.github.com>
2021-07-23 17:00:57 -0300

    feat: Starting the base of the Eluf project

---
+++ src/main/java/com/callforcode/eluf/ElufApplication.java
@@ -0,0 +1,13 @@
+package com.callforcode.eluf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElufApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElufApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/callforcode/eluf/ElufApplicationTests.java
@@ -0,0 +1,13 @@
+package com.callforcode.eluf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElufApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
