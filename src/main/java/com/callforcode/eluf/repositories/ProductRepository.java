f2f2e9f019ef3d233bd04ba9ab9835d33f7c67c9
PedroHBCruz <75803558+PedroHBCruz@users.noreply.github.com>
2021-07-24 15:44:52 -0300

    fix: Setting the method name contained in ProductRepository

--- src/main/java/com/callforcode/eluf/repositories/ProductRepository.java
+++ src/main/java/com/callforcode/eluf/repositories/ProductRepository.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,6 +16,6 @@ import com.callforcode.eluf.model.Product;
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 
 	@Transactional(readOnly=true)
-	@Query("SELECT DISTINCT obj FROM Product obj INNER JOIN obj.categories cat WHERE obj.name LIKE %:name% AND cat IN :categories")
+	//@Query("SELECT DISTINCT obj FROM Product obj INNER JOIN obj.categories cat WHERE obj.name LIKE %:name% AND cat IN :categories")
 	Page<Product> findDistinctByNameContainingAndCategoriesIn(@Param("name") String name, @Param("categories") List<Category> categories, Pageable pageRequest);
 }
