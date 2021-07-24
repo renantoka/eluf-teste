ec80df9f4b895f7be6a69f9378283bc3f9f0da07
PedroHBCruz <75803558+PedroHBCruz@users.noreply.github.com>
2021-07-24 12:59:00 -0300

    feat: Creating classes from the exceptions package that will be used in the services package

---
+++ src/main/java/com/callforcode/eluf/services/exceptions/AuthorizationException.java
@@ -0,0 +1,13 @@
+package com.callforcode.eluf.services.exceptions;
+
+public class AuthorizationException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public AuthorizationException(String message) {
+		super(message);
+	}
+
+	public AuthorizationException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
