97fedfa8ebfcbd4ce6d9a5c68953fc2768aa9d7b
Renan Tokashiki <61675315+renantoka@users.noreply.github.com>
2021-07-25 18:01:43 -0400

    Update UserService.java

--- src/main/java/com/callforcode/eluf/services/UserService.java
+++ src/main/java/com/callforcode/eluf/services/UserService.java
@@ -1,10 +1,11 @@
 package com.callforcode.eluf.services;
 
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
 
 import com.callforcode.eluf.security.UserSS;
 
-
+@Service
 public class UserService {
 	
 	public static UserSS authenticated() {
