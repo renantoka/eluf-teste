9ab05bb18528bb2d63c62da56cac8a0983406b7d
Renan Tokashiki <renantoka@icloud.com>
2021-07-25 14:39:06 -0400

    fix: Correction of last commit

--- src/main/java/com/callforcode/eluf/ElufApplication.java
+++ src/main/java/com/callforcode/eluf/ElufApplication.java
@@ -1,25 +1,21 @@
 package com.callforcode.eluf;
 
-import java.util.Locale;
-
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.i18n.FixedLocaleResolver;
 
 
 
 @SpringBootApplication
-public class ElufApplication {
+public class ElufApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ElufApplication.class, args);
 	}
 
-	@Bean
-	public LocaleResolver localeResolver() {
-		return new FixedLocaleResolver(new Locale("pt", "BR"));
+	@Override
+	public void run(String... args) throws Exception {
+			
 	}
 
 }
