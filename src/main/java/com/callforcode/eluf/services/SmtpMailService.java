8ae0a563d56f188edfac60d9fd64140a00de8e78
Renan Tokashiki <61675315+renantoka@users.noreply.github.com>
2021-07-25 18:01:59 -0400

    Update SmtpMailService.java

--- src/main/java/com/callforcode/eluf/services/SmtpMailService.java
+++ src/main/java/com/callforcode/eluf/services/SmtpMailService.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
 
+import org.springframework.stereotype.Service;
+@Service
 public class SmtpMailService extends AbstractMailService{
 
 	@Autowired
