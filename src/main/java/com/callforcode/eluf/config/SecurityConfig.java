43f4bf0c29a3ce1fa25b3897a316ccd751839bf7
PedroHBCruz <75803558+PedroHBCruz@users.noreply.github.com>
2021-07-25 01:20:44 -0300

    fix: Fixing SecurityConfig class syntax error

--- src/main/java/com/callforcode/eluf/config/SecurityConfig.java
+++ src/main/java/com/callforcode/eluf/config/SecurityConfig.java
@@ -47,7 +47,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	private static final String[] PUBLIC_MATCHERS_GET = {
 			"/products/**",
 			"/categories/**",
-			"/states/**"
+			"/state/**"
 	};
 	
 	private static final String[] PUBLIC_MATCHERS_POST = {
