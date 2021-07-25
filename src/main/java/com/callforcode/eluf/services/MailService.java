23305c79ec5b2a5ce22ffdd883d1c732de5b4ef0
Renan Tokashiki <61675315+renantoka@users.noreply.github.com>
2021-07-25 17:50:58 -0400

    Update MailService.java

--- src/main/java/com/callforcode/eluf/services/MailService.java
+++ src/main/java/com/callforcode/eluf/services/MailService.java
@@ -1,10 +1,12 @@
 package com.callforcode.eluf.services;
 
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.stereotype.Service;
 
 import com.callforcode.eluf.model.Client;
 import com.callforcode.eluf.model.Order;
 
+@Service
 public interface MailService {
 
 	void sendOrderConfirmationMail(Order obj);
