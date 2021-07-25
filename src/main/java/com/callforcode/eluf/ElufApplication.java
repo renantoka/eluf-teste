c191e43f218b31992d295465d4546c990dd6835f
Renan Tokashiki <renantoka@icloud.com>
2021-07-25 14:34:43 -0400

    fix: updating config to deploy

--- src/main/java/com/callforcode/eluf/ElufApplication.java
+++ src/main/java/com/callforcode/eluf/ElufApplication.java
@@ -1,21 +1,25 @@
 package com.callforcode.eluf;
 
-import org.springframework.boot.CommandLineRunner;
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.FixedLocaleResolver;
 
 
 
 @SpringBootApplication
-public class ElufApplication implements CommandLineRunner {
+public class ElufApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ElufApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-			
+	@Bean
+	public LocaleResolver localeResolver() {
+		return new FixedLocaleResolver(new Locale("pt", "BR"));
 	}
 
 }
